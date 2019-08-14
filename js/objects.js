29f36c975edee2c836418a3cfb40ce8bfe3f1ca1
Robin McKenna <robin.mckenna@magic42.co.uk>
2019-08-14 13:23:41 +0100

    Add js/objects.js #3

---
+++ js/objects.js
@@ -0,0 +1,13 @@
+var person = new Object();
+
+// Add properties and methods
+
+person["firstname"] = "Steve";
+person["lastname"] = "Smith";
+
+var firstNameProperty = "firstname";
+
+console.log(person);
+console.log(person["firstname"]);
+
+console.log(person.firstname);
