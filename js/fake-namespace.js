2ca6f0ea222c85cf9a7622e31a604c5dcfac83b9
Robin McKenna <robin.mckenna@magic42.co.uk>
2019-08-21 09:23:07 +0100

    Add faking namespaces code and add script src #3

---
+++ js/fake-namespace.js
@@ -0,0 +1,22 @@
+// English Namespace
+
+var english = {};
+
+var english = {
+    greetings: {
+        basic: 'Hello'
+    }
+};
+
+english.greet = 'Hello';
+english.greetings.morning = "Good Morning";
+
+// Spanish Namespace
+
+var spanish = {};
+
+spanish.greet = 'Hola!';
+
+console.log(english);
+
+console.log(spanish);
