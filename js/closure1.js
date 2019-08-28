2a6adb8724b55e57871342ea810a3b8607fdda65
Robin McKenna <robin.mckenna@magic42.co.uk>
2019-08-28 10:07:29 +0100

    Add closure example #5
    + Add link to html
    + Add examples

---
+++ js/closure1.js
@@ -0,0 +1,57 @@
+// Example 1
+
+function greet(whatToSay) {
+    return function(name) {
+        console.log(whatToSay + name);
+    }
+}
+
+greet('Hi ')('Steve');
+
+var sayHi = greet('Hi');
+sayHi();
+
+
+// Example 2
+
+function buildFunctions() {
+    var arr = [];
+
+    for (var i = 0; i < 3; i++) {
+        arr.push(
+            function() {
+                console.log(i);
+            }
+        )
+    }
+
+    return arr;
+}
+
+var fs = buildFunctions();
+
+fs[0]();
+fs[1]();
+fs[2]();
+
+function buildFunctions2() {
+    var arr = [];
+
+    for (var i = 0; i < 3; i++) {
+        arr.push(
+            (function(j) {
+                return function() {
+                    console.log(j);
+                }
+            }(i))
+        )
+    }
+
+    return arr;
+}
+
+var fs2 = buildFunctions2();
+
+fs2[0]();
+fs2[1]();
+fs2[2]();
