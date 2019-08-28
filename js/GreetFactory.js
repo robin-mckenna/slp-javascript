7afa8ead4cae8b921d7d79334a1f2e14f2598bc3
Robin McKenna <robin.mckenna@magic42.co.uk>
2019-08-28 10:24:15 +0100

    Add GreetFacotry Example #5

---
+++ js/GreetFactory.js
@@ -0,0 +1,19 @@
+function makeGreeting(language) {
+
+    return function(firstName, lastName) {
+        if (language == 'en') {
+            console.log('Hello ' + firstName + ' ' + lastName);
+        }
+
+        if (language == 'es') {
+            console.log('Hola ' + firstName + ' ' + lastName);
+        }
+
+    }
+}
+
+var greetEnglish = makeGreeting('en');
+var greetSpanish = makeGreeting('es');
+
+greetEnglish('John', 'Doe');
+greetSpanish('John', 'Doe');
