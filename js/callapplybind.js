df8b13bade6bc341983855051ea033a147c4b05c
Robin McKenna <robin.mckenna@magic42.co.uk>
2019-08-28 12:28:59 +0100

    Add callapplybind examples #5

---
+++ js/callapplybind.js
@@ -0,0 +1,35 @@
+var person = {
+    firstName: "John",
+    lastName: "Doe",
+    getFullName: function() {
+        var fullName = this.firstName + ' ' + this.lastName;
+        return fullName;
+    }
+}
+
+var logName = function(lang1, lang2) {
+    console.log("Logged: " + this.getFullName());
+    console.log("Arguments: " + lang1 + " " + lang2);
+    console.log("-----------------------------------");
+}.bind(person);
+
+// var logPersonName = logName.bind(person);
+
+console.log(logName);
+logName();
+// logPersonName();
+
+logName.call(person, 'en', 'es');
+
+logName.apply(person, ['en', 'es']);
+
+// Function Borrowing
+
+var person2 = {
+    firstName: "Jane",
+    lastName: "Doe",
+}
+
+var borrowing = person.getFullName.apply(person2);
+
+console.log(borrowing);
