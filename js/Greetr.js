69cd9c712ac3e3da79d7346f963271c0ca9b4f82
Robin McKenna <robin.mckenna@magic42.co.uk>
2019-09-11 12:09:30 +0100

    Add mini framework #7 WIP

--- js/Greetr.js
+++ js/Greetr.js
@@ -1,1 +1,70 @@
-console.log("Course Output");
+// Execution Context
+
+(function(global, $) {
+    var Greetr = function(firstName, lastName, language) {
+        return new Greetr.init(firstName, lastName, language);
+    }
+
+    var supportedLangs = ['en','es'];
+
+    var greetings = {
+        en: 'Hello',
+        es: 'Hola'
+    };
+
+    var formalGreetings = {
+        en: 'Greetings',
+        es: 'Saludos'
+    };
+
+    var logMessages = {
+        en: 'Logged In',
+        es: 'Inicio session'
+    }
+
+    Greetr.prototype = {
+        fullName: function() {
+            return this.firstName + ' ' + this.lastName;
+        },
+
+        validate: function() {
+            if (supportedLangs.indexOf(this.language) === -1) {
+                throw "Invalid language";
+            }
+        },
+
+        greeting: function() {
+            return greetings[this.language] + ' ' + this.firstName + '!';
+        },
+
+        formalGreetings: function() {
+            return formalGreetings([this.language]) + ', ' this.fullName;
+        },
+
+        // greet: function(formal) {
+        //     var msg;
+        //
+        //     if(formal) {
+        //         msg = this.formalGreetings();
+        //     } else {
+        //         msg = this.greeting();
+        //     }
+        //
+        //     if (console) {
+        //         console.log(msg);
+        //     }
+        // }
+    }
+
+    Greetr.init = function(firstName, lastName, language) {
+        var self = this;
+        self.firstName = firstName || '';
+        self.lastName = lastName || '';
+        self.language = language || 'en';
+    }
+
+    Greetr.init.prototype = Greetr.prototype;
+
+    global.Greetr = global.G$ = Greetr;
+
+}(window, jQuery));
---
+++ js/app.js
@@ -0,0 +1,6 @@
+$(function(){
+    var g = G$('John', 'Doe');
+    console.log(g);
+
+    console.log(g.fullName());
+});
