097a273bd8707eaabe217f43022bf22b516483ed
Robin McKenna <robin.mckenna@magic42.co.uk>
2019-09-11 09:31:07 +0100

    Add initialization example #6

---
+++ js/initialization.js
@@ -0,0 +1,22 @@
+var data = [
+    {
+        checked: false,
+        dimensions: {
+            width: 5,
+            height: 10
+        },
+        id: 1,
+        name: "A green door",
+        price: 12.5,
+        tags: [
+            "home",
+            "green"
+        ]
+    },
+    {
+        name: "A blue door",
+        checked: true
+    }
+]
+
+console.log(data);
